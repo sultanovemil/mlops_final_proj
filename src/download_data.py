b769255ad5bf68d22dc8b8f225a4fe866a51b289
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2024-05-12 18:26:10 +0500

    Change file id

--- src/download_data.py
+++ src/download_data.py
@@ -1,14 +1,16 @@
 import requests
 
+
+# Download a dataset file from Google drive
 def download_csv_from_google_drive(file_id, destination):
     url = f"https://drive.google.com/uc?id={file_id}"
     response = requests.get(url)
-    
+
     with open(destination, 'wb') as f:
         f.write(response.content)
 
-# Замените 'YOUR_FILE_ID' на фактический ID файла CSV на Google Диске
-file_id = '1nN6LvyvIquzWBtNktwvK8Zdn7DnbSJd5'
+
+file_id = '1grulrrGM2CQIDt-d1vI6Y-4VklAjc8Jv'
 destination = 'data/raw/mushroom_dataset.csv'
 
 download_csv_from_google_drive(file_id, destination)
